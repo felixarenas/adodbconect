3165b901471bc07d322d77e47ec7584bb6c9884f
felix arenas lourido <felixarenas7@gmail.com>
2018-02-19 17:58:46 -0500

    Crea comando para laravel

---
+++ src/Console/Commands/AdodbInitConfigCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace farenas\AdodbConect\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class AdodbInitConfigCommand extends Command
+{
+
+    protected $signature = 'adodb:ini';
+    protected $description = 'Genera el archivo de configuracion para adodbConect';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $key = $this->getRandomKey();
+
+        $path = config_path('dbConfig.php');
+        print($path); die();
+        if (file_exists($path)) {
+            file_put_contents($path, str_replace(
+                $this->laravel['config']['dbConfig.secret'], $key, file_get_contents($path)
+            ));
+        }
+
+        $this->laravel['config']['dbConfig.secret'] = $key;
+
+        $this->info("jwt-auth secret [$key] set successfully.");
+    }
+}
