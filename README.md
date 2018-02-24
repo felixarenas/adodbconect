62b46d1054f5ef2bbbdd950a327b51a87ffc993b
felix arenas lourido <felixarenas7@gmail.com>
2018-02-23 19:35:57 -0500

    Ajusta Instructivo

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # adodbconect
-ADODBCONECT
-Facilitates the use of adodb php in laravel
+ADODBCONECT Facilitates the use of adodb php in laravel
+
+## Install
+
+Via Composer
+
+``` bash
+$ composer require felixarenas/adodbconect
+```
+
+## Providers
+
+``` php
+farenas\AdodbConect\Providers\AdodbConectServiceProvider::class,
+
+'AdodbConect' => farenas\AdodbConect\Facades\AdodbConectFacade::class,
+```
+
+## Publish
+
+``` php
+php artisan vendor:publish --provider="farenas\AdodbConect\Providers\AdodbConectServiceProvider"
+```
+
+## Usage
+
+``` php
+use AdodbConect;
+
+$paramAdoDb = [
+    'cursor' => true,
+    'plsql'  => 'PackageOracle.function_package(:codUser, :codCompany);',
+    'datos'  => [
+        'codUser' => 2222,
+        'codCompany' => 6655
+    ]
+];
+
+AdodbConect::param($paramAdoDb)->run();
+```
--- src/Console/Commands/AdodbInitConfigCommand.php
+++ src/Console/Commands/AdodbInitConfigCommand.php
@@ -7,12 +7,13 @@ use Illuminate\Console\Command;
 class AdodbInitConfigCommand extends Command
 {
 
-    protected $signature = 'adodb:ini';
+    protected $signature = 'adodb:clearcache';
 
-    protected $description = 'Genera el archivo de configuracion para adodbConect';
+    protected $description = 'Elimina cache de AdodbConect';
 
     public function __construct()
     {
+
         parent::__construct();
     }
 
@@ -24,18 +25,18 @@ class AdodbInitConfigCommand extends Command
     public function handle()
     {
 
-        $key = $this->getRandomKey();
+        // $key = $this->getRandomKey();
 
-        $path = config_path('dbConfig.php');
-        print($path); die();
-        if (file_exists($path)) {
-            file_put_contents($path, str_replace(
-                $this->laravel['config']['dbConfig.secret'], $key, file_get_contents($path)
-            ));
-        }
+        // $path = config_path('dbConfig.php');
+        // print($path); die();
+        // if (file_exists($path)) {
+        //     file_put_contents($path, str_replace(
+        //         $this->laravel['config']['dbConfig.secret'], $key, file_get_contents($path)
+        //     ));
+        // }
 
-        $this->laravel['config']['dbConfig.secret'] = $key;
+        // $this->laravel['config']['dbConfig.secret'] = $key;
 
-        $this->info("jwt-auth secret [$key] set successfully.");
+        $this->info("Cache eliminada con exito.");
     }
 }
